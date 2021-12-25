71dca1c466b5558e6e633e99452ec623f1d33fd2
Adrian <A01242469@itesm.mx>
2021-12-25 14:45:39 -0700

    header

---
+++ av-ang/src/app/usr-home/usr-home.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsrHomeComponent } from './usr-home.component';
+
+describe('UsrHomeComponent', () => {
+  let component: UsrHomeComponent;
+  let fixture: ComponentFixture<UsrHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsrHomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsrHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
